4d27437f19c257f7da163194f5ece0b8ec1aa311
Cory Holenstein <cholensteinwork@gmail.com>
2022-03-02 20:50:03 -0500

    first commit

---
+++ sharp-front-end2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sharp_front_end2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
